ade39ff75131a736403e67e6fe8c7d07788f65c4
heitor1240 <111541276+heitor1240@users.noreply.github.com>
2024-06-26 21:47:06 -0300

    atualização1.2

--- script.js
+++ script.js
@@ -3,22 +3,26 @@ function sendToWhatsApp() {
     const nome = document.getElementById('nome').value;
     const email = document.getElementById('email').value;
     const msg = document.getElementById('msg').value;
-    
+    const numeroWhatsApp1 = document.getElementById('numeroWhatsApp1').value;
 
     // Verifique se todos os campos estão preenchidos
-    if (!nome || !email || !msg) {
+    if (!nome || !email || !msg || !numeroWhatsApp1) {
         alert('Por favor, preencha todos os campos.');
         return;
     }
 
     // Formate a mensagem para enviar
-    const mensagem = `Nome: ${nome}\nEmail: ${email}\nMensagem: ${msg}`;
+    const mensagem = `*Solicitação de agendamento*:\n\nNome: ${nome}\nEmail: ${email}\nNúmero${numeroWhatsApp1}\nMensagem: ${msg}`;
 
     // Codifique a mensagem para URL
     const mensagemEncoded = encodeURIComponent(mensagem);
 
     // Número do WhatsApp (use o formato internacional sem espaços ou símbolos, apenas números)
     const numeroWhatsApp = '5547988831423';
+
+      // Formate o número do WhatsApp (remova espaços, traços e parênteses)
+      const numeroFormatado = numeroWhatsApp.replace(/\D/g, '');
+      
 
     // Crie a URL do WhatsApp
     const url = `https://wa.me/${numeroWhatsApp}?text=${mensagemEncoded}`;
